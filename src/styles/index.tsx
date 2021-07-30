55b4ac5c10c60faeb9aef6cd341931c6cb6393fd
seockhun <hun00719@gmail.com>
2021-07-30 13:12:52 +0900

    [ADD] FILE setting

--- src/styles/index.tsx
+++ src/styles/index.tsx
@@ -1,1 +1,15 @@
-export const COLOR = "red";
+export const COLOR = {
+    mainTextColor: "#000000",
+
+    whiteColor: "#ffffff",
+
+    negativeColor: "#FF2525",
+
+    lateColor: "#30A8FF"
+};
+
+export const WIDTH = {
+    mainWidth: "1200px",
+
+    subWidth: "500px",
+}
