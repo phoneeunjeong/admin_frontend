69d920a48a52e8630740e445aca112b194d9b1f9
seockhun <hun00719@gmail.com>
2021-07-30 14:58:54 +0900

    [FIX] App.tsx 코드 추가 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import GlobalStyle from "./styles/globalStyle";
 import { Switch } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import Header from "./components/header/index";
 
 
 export default function App() {
@@ -10,7 +9,7 @@ export default function App() {
     <Router>
       <GlobalStyle />
       <Switch>
-        <Header></Header>
+
       </Switch>
     </Router>
   );
